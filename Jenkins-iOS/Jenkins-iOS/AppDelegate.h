9e75a35f4bf3d56408de0b32525733b37fc2197a
RAJESH KUMAR <rajeshkumary@dbs.com>
2017-09-08 11:23:34 +0530

    Initial commit

---
+++ Jenkins-iOS/Jenkins-iOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Jenkins-iOS
+//
+//  Created by admin on 08/09/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Jenkins-iOS/Jenkins-iOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Jenkins-iOS
+//
+//  Created by admin on 08/09/17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
